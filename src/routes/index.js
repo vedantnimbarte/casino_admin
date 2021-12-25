857c1c63bf425f38f7fc60bb345a4971901ef67f
Mount Game <mountgame@Mounts-Mac-mini.local>
2021-12-25 12:46:34 +0530

    validating routes permissions and showing appropriate routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,20 @@ import { useRoutes } from 'react-router-dom';
 import MainRoutes from './MainRoutes';
 import AuthenticationRoutes from './AuthenticationRoutes';
 
+import { useSelector } from 'react-redux';
+
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
-    console.log(localStorage.getItem('user-authenticated') ? 'authenticated' : 'Login');
+    const user = useSelector((state) => state.user);
+
+    MainRoutes.children.map((route, index) => {
+        if (!user.allowedRoutes.includes(route.path)) {
+            MainRoutes.children.splice(index, 1);
+        }
+        return route;
+    });
+
     // return useRoutes(localStorage.getItem('user-authenticated') ? [MainRoutes] : [AuthenticationRoutes]);
     return useRoutes([AuthenticationRoutes, MainRoutes]);
 }

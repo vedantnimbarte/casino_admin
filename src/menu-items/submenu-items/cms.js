1a1a5a6d7c7078be663aa93aced169883e0310ed
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-27 15:36:13 +0530

    Updated url of all cms group routes

--- src/menu-items/submenu-items/cms.js
+++ src/menu-items/submenu-items/cms.js
@@ -3,70 +3,70 @@ const cmsMenuItems = [
         id: 'slider',
         title: 'Slider',
         type: 'item',
-        url: '/slider',
+        url: '/cms/slider',
         breadcrumbs: false
     },
     {
         id: 'games',
         title: 'Games',
         type: 'item',
-        url: '/game',
+        url: '/cms/game',
         breadcrumbs: false
     },
     {
         id: 'games-group',
         title: 'Game Groups',
         type: 'item',
-        url: '/game-group',
+        url: '/cms/game-group',
         breadcrumbs: false
     },
     {
         id: 'faq',
         title: 'FAQ',
         type: 'item',
-        url: '/faq',
+        url: '/cms/faq',
         breadcrumbs: false
     },
     {
         id: 'notification',
         title: 'Notifications',
         type: 'item',
-        url: '/notification',
+        url: '/cms/notification',
         breadcrumbs: false
     },
     {
         id: 'terms-conditions',
         title: 'Terms & Conditions',
         type: 'item',
-        url: '/terms-conditions',
+        url: '/cms/terms-conditions',
         breadcrumbs: false
     },
     {
         id: 'payment-terms',
         title: 'Payment Terms',
         type: 'item',
-        url: '/payment-terms',
+        url: '/cms/payment-terms',
         breadcrumbs: false
     },
     {
         id: 'privacy-policy',
         title: 'Privacy Policy',
         type: 'item',
-        url: '/privacy-policy',
+        url: '/cms/privacy-policy',
         breadcrumbs: false
     },
     {
         id: 'about-us',
         title: 'About Us',
         type: 'item',
-        url: '/about',
+        url: '/cms/about',
         breadcrumbs: false
     },
     {
         id: 'disclaimer',
         title: 'Disclaimer',
         type: 'item',
-        url: '/disclaimer',
+        url: '/cms/disclaimer',
         breadcrumbs: false
     }
 ];

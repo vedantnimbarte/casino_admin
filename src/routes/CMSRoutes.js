9b7813b03c9491ca4c2f4cfc6898a263776c0b05
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-27 15:37:02 +0530

    created separate routing file for cms routes

---
+++ src/routes/CMSRoutes.js
@@ -0,0 +1,74 @@
+import { lazy } from 'react';
+
+// project imports
+import MainLayout from 'layout/MainLayout';
+import Loadable from 'ui-component/Loadable';
+
+// cms routing
+const FAQPage = Loadable(lazy(() => import('views/pages/cms/faq')));
+const GamesPage = Loadable(lazy(() => import('views/pages/cms/games')));
+const SliderPage = Loadable(lazy(() => import('views/pages/cms/slider')));
+const NotificationPage = Loadable(lazy(() => import('views/pages/cms/notification')));
+const TermsAndConditionsPage = Loadable(lazy(() => import('views/pages/cms/termsAndConditions')));
+const DisclaimerPage = Loadable(lazy(() => import('views/pages/cms/disclaimer')));
+const AboutUsPage = Loadable(lazy(() => import('views/pages/cms/aboutUs')));
+const PaymentTermsPage = Loadable(lazy(() => import('views/pages/cms/paymentTerms')));
+const PrivacyPolicyPage = Loadable(lazy(() => import('views/pages/cms/privacyPolicy')));
+const GameGroupPage = Loadable(lazy(() => import('views/pages/cms/gameGroup')));
+
+const AuthRegister3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Register3')));
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const CMSRoutes = {
+    path: '/cms',
+    element: <MainLayout />,
+    children: [
+        {
+            path: '/faq',
+            element: <FAQPage />
+        },
+        {
+            path: '/game',
+            element: <GamesPage />
+        },
+        {
+            path: '/game-group',
+            element: <GameGroupPage />
+        },
+        {
+            path: '/slider',
+            element: <SliderPage />
+        },
+        {
+            path: '/notification',
+            element: <NotificationPage />
+        },
+        {
+            path: '/privacy-policy',
+            element: <PrivacyPolicyPage />
+        },
+        {
+            path: '/about',
+            element: <AboutUsPage />
+        },
+        {
+            path: '/disclaimer',
+            element: <DisclaimerPage />
+        },
+        {
+            path: '/payment-terms',
+            element: <PaymentTermsPage />
+        },
+        {
+            path: '/terms-conditions',
+            element: <TermsAndConditionsPage />
+        },
+        {
+            path: '*',
+            element: <AuthRegister3 />
+        }
+    ]
+};
+
+export default CMSRoutes;

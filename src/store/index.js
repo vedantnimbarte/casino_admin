a741a15cb70db5eb2fda1362e19901ef9b0a0ba8
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-27 12:22:03 +0530

    removed unused code from store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,3 @@
-import { createStore } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 // import reducer from './reducer';
 
@@ -7,8 +6,6 @@ import customizationReducer from './customizationReducer';
 import userReducer from './userReducer';
 
 // ==============================|| REDUX - MAIN STORE ||============================== //
-
-// const store = createStore(reducer);
 
 export default configureStore({
     reducer: {

e6ed6217b39e6129a31927252479ede38673ace1
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-28 15:22:33 +0530

    Commented multiple pages

--- src/menu-items/submenu-items/cms.js
+++ src/menu-items/submenu-items/cms.js
@@ -1,11 +1,11 @@
 const cmsMenuItems = [
-    {
-        id: 'slider',
-        title: 'Slider',
-        type: 'item',
-        url: '/cms/slider',
-        breadcrumbs: false
-    },
+    // {
+    //     id: 'slider',
+    //     title: 'Slider',
+    //     type: 'item',
+    //     url: '/cms/slider',
+    //     breadcrumbs: false
+    // },
     {
         id: 'games',
         title: 'Games',
@@ -19,56 +19,56 @@ const cmsMenuItems = [
         type: 'item',
         url: '/cms/game-group',
         breadcrumbs: false
-    },
-    {
-        id: 'faq',
-        title: 'FAQ',
-        type: 'item',
-        url: '/cms/faq',
-        breadcrumbs: false
-    },
-    {
-        id: 'notification',
-        title: 'Notifications',
-        type: 'item',
-        url: '/cms/notification',
-        breadcrumbs: false
-    },
-    {
-        id: 'terms-conditions',
-        title: 'Terms & Conditions',
-        type: 'item',
-        url: '/cms/terms-conditions',
-        breadcrumbs: false
-    },
-    {
-        id: 'payment-terms',
-        title: 'Payment Terms',
-        type: 'item',
-        url: '/cms/payment-terms',
-        breadcrumbs: false
-    },
-    {
-        id: 'privacy-policy',
-        title: 'Privacy Policy',
-        type: 'item',
-        url: '/cms/privacy-policy',
-        breadcrumbs: false
-    },
-    {
-        id: 'about-us',
-        title: 'About Us',
-        type: 'item',
-        url: '/cms/about',
-        breadcrumbs: false
-    },
-    {
-        id: 'disclaimer',
-        title: 'Disclaimer',
-        type: 'item',
-        url: '/cms/disclaimer',
-        breadcrumbs: false
     }
+    // {
+    //     id: 'faq',
+    //     title: 'FAQ',
+    //     type: 'item',
+    //     url: '/cms/faq',
+    //     breadcrumbs: false
+    // },
+    // {
+    //     id: 'notification',
+    //     title: 'Notifications',
+    //     type: 'item',
+    //     url: '/cms/notification',
+    //     breadcrumbs: false
+    // },
+    // {
+    //     id: 'terms-conditions',
+    //     title: 'Terms & Conditions',
+    //     type: 'item',
+    //     url: '/cms/terms-conditions',
+    //     breadcrumbs: false
+    // },
+    // {
+    //     id: 'payment-terms',
+    //     title: 'Payment Terms',
+    //     type: 'item',
+    //     url: '/cms/payment-terms',
+    //     breadcrumbs: false
+    // },
+    // {
+    //     id: 'privacy-policy',
+    //     title: 'Privacy Policy',
+    //     type: 'item',
+    //     url: '/cms/privacy-policy',
+    //     breadcrumbs: false
+    // },
+    // {
+    //     id: 'about-us',
+    //     title: 'About Us',
+    //     type: 'item',
+    //     url: '/cms/about',
+    //     breadcrumbs: false
+    // },
+    // {
+    //     id: 'disclaimer',
+    //     title: 'Disclaimer',
+    //     type: 'item',
+    //     url: '/cms/disclaimer',
+    //     breadcrumbs: false
+    // }
 ];
 
 export default cmsMenuItems;

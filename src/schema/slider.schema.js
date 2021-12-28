1aaf7c4a8081143358d1d4b63c6d30f82ec92d85
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-28 17:00:04 +0530

    updated slider schema and added image validation instead of url

--- src/schema/slider.schema.js
+++ src/schema/slider.schema.js
@@ -1,12 +1,17 @@
 import * as yup from 'yup';
 
+const SUPPORTED_FILETYPES = ['jpeg', 'png', 'jpg'];
+
 const sliderSchema = yup.object().shape({
     name: yup.string().max(100, 'Name should be maximum 100 characters').required('Please enter the game name'),
-    url: yup
-        .string()
-        .url('Please enter valid game url')
-        .max(255, 'URL should be maximum 255 characters')
-        .required('Please enter the game url')
+    image: yup
+        .mixed()
+        .required('Please select game image')
+        .test(
+            'filetype',
+            'Only .jpg, .jpeg, .png files are supported',
+            (value) => !value || (value && SUPPORTED_FILETYPES.includes(value.split('.').slice(-1)[0]))
+        )
 });
 
 export default sliderSchema;

d96ade7767c3494641b636cbefa278089d5ab9ff
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-29 19:02:35 +0530

    Created new component for showing total users in network

---
+++ src/views/dashboard/Default/TotalNetworkCard.js
@@ -0,0 +1,128 @@
+import PropTypes from 'prop-types';
+
+// material-ui
+import { useTheme, styled } from '@mui/material/styles';
+import { Avatar, Box, Grid, Typography } from '@mui/material';
+
+import { IconDeviceAnalytics as NetworkIcon } from '@tabler/icons';
+
+// third-party
+import Chart from 'react-apexcharts';
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+import SkeletonTotalOrderCard from 'ui-component/cards/Skeleton/EarningCard';
+
+const CardWrapper = styled(MainCard)(({ theme }) => ({
+    backgroundColor: theme.palette.primary.dark,
+    color: '#fff',
+    overflow: 'hidden',
+    position: 'relative',
+    '&>div': {
+        position: 'relative',
+        zIndex: 5
+    },
+    '&:after': {
+        content: '""',
+        position: 'absolute',
+        width: 210,
+        height: 210,
+        background: theme.palette.primary[800],
+        borderRadius: '50%',
+        zIndex: 1,
+        top: -85,
+        right: -95,
+        [theme.breakpoints.down('sm')]: {
+            top: -105,
+            right: -140
+        }
+    },
+    '&:before': {
+        content: '""',
+        position: 'absolute',
+        zIndex: 1,
+        width: 210,
+        height: 210,
+        background: theme.palette.primary[800],
+        borderRadius: '50%',
+        top: -125,
+        right: -15,
+        opacity: 0.5,
+        [theme.breakpoints.down('sm')]: {
+            top: -155,
+            right: -70
+        }
+    }
+}));
+
+// ==============================|| DASHBOARD - TOTAL ORDER LINE CHART CARD ||============================== //
+
+const TotalNetworkCard = ({ isLoading }) => {
+    const theme = useTheme();
+
+    return (
+        <>
+            {isLoading ? (
+                <SkeletonTotalOrderCard />
+            ) : (
+                <CardWrapper border={false} content={false} elevation={4}>
+                    <Box sx={{ p: 2.25 }}>
+                        <Grid container direction="column">
+                            <Grid item>
+                                <Grid container justifyContent="space-between">
+                                    <Grid item>
+                                        <Avatar
+                                            variant="rounded"
+                                            sx={{
+                                                ...theme.typography.commonAvatar,
+                                                ...theme.typography.largeAvatar,
+                                                backgroundColor: theme.palette.primary[800],
+                                                color: '#fff',
+                                                mt: 1
+                                            }}
+                                        >
+                                            <NetworkIcon />
+                                        </Avatar>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                            <Grid item sx={{ mb: 0.75 }}>
+                                <Grid container alignItems="center">
+                                    <Grid item xs={6}>
+                                        <Grid container alignItems="center">
+                                            <Grid item>
+                                                <Typography sx={{ fontSize: '2.125rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>
+                                                    35
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <Typography
+                                                    sx={{
+                                                        fontSize: '1rem',
+                                                        fontWeight: 500,
+                                                        color: theme.palette.primary[200]
+                                                    }}
+                                                >
+                                                    Total Network
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                    {/* <Grid item xs={6}>
+                                        <Chart {...ChartDataMonth} />
+                                    </Grid> */}
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </CardWrapper>
+            )}
+        </>
+    );
+};
+
+TotalNetworkCard.propTypes = {
+    isLoading: PropTypes.bool
+};
+
+export default TotalNetworkCard;

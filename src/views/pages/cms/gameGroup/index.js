5033d77cfee8ccac652ec3f4ae3500f396ac378e
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-29 19:04:41 +0530

    Showing form in custom full screen dialog component and validating using useFormik hook react. Passing required values to full page dialog component using props

--- src/views/pages/cms/gameGroup/index.js
+++ src/views/pages/cms/gameGroup/index.js
@@ -1,27 +1,42 @@
-import { useState } from 'react';
-import { IconButton, Tooltip, Box, Button, InputLabel, TextField, FormControl } from '@mui/material';
+import React, { useState } from 'react';
+import { IconButton, Tooltip, Box, TextField } from '@mui/material';
 import { IconCirclePlus as AddIcon } from '@tabler/icons';
-import { Form, Formik } from 'formik';
+import { useFormik } from 'formik';
 import gameGroupSchema from 'schema/gameGroup.schema';
 
 // Components
 import MainCard from '../../../../ui-component/cards/MainCard';
 import DataTable from 'components/DataTable';
-import Modal from 'components/Modal';
 import NotFoundCard from 'components/NotFoundCard';
+import FullScreenDialog from 'components/FullScreenDialog';
 
 function GameGroup() {
     const [openModal, setOpenModal] = useState(false);
 
-    const columns = ['ID', 'Name', 'Action'];
+    const formik = useFormik({
+        initialValues: { name: '', description: '' },
+        validationSchema: gameGroupSchema,
+        onSubmit: (values) => {
+            console.log(values);
+        },
+        onReset: (values) => {
+            values = { name: '', description: '' };
+        }
+    });
 
-    const data = [];
+    const columns = ['ID', 'Group', 'Action'];
+
+    const data = [
+        {
+            ID: 1,
+            Group: 'Fish'
+        }
+    ];
 
     const options = {
         filter: false,
         print: false,
         download: false,
-        search: false,
         selectableRows: false,
         rowsPerPage: 10,
         rowsPerPageOptions: [10, 20]
@@ -48,72 +63,37 @@ function GameGroup() {
                 </Box>
             </MainCard>
 
-            <Modal title="Add New Game Group" open={openModal} onClose={() => setOpenModal(!openModal)}>
-                <Box style={{ display: 'flex', flexDirection: 'column' }}>
-                    <Formik
-                        initialValues={{ name: '', description: '' }}
-                        validationSchema={gameGroupSchema}
-                        onSubmit={(values) => console.log(values)}
-                    >
-                        {(formik) => (
-                            <Form noValidate onSubmit={formik.handleSubmit}>
-                                <FormControl fullWidth style={{ marginTop: 10, marginBottom: 10 }}>
-                                    <InputLabel>Game Group Name</InputLabel>
-                                    <TextField
-                                        value={formik.values.name}
-                                        type="text"
-                                        name="name"
-                                        label="Game Group Name"
-                                        onChange={formik.handleChange}
-                                        variant="outlined"
-                                        error={formik.touched.name && Boolean(formik.errors.name)}
-                                        helperText={formik.touched.name && formik.errors.name}
-                                        required
-                                    />
-                                </FormControl>
-                                <FormControl fullWidth style={{ marginTop: 10, marginBottom: 10 }}>
-                                    <InputLabel>Description</InputLabel>
-                                    <TextField
-                                        value={formik.values.description}
-                                        type="text"
-                                        name="description"
-                                        label="Description"
-                                        onChange={formik.handleChange}
-                                        variant="outlined"
-                                        error={formik.touched.description && Boolean(formik.errors.description)}
-                                        helperText={formik.touched.description && formik.errors.description}
-                                    />
-                                </FormControl>
-                                <Box style={{ display: 'flex' }}>
-                                    <Button
-                                        type="submit"
-                                        style={{
-                                            backgroundColor: '#673AB7',
-                                            color: '#fff',
-                                            margin: 10,
-                                            width: '50%',
-                                            alignSelf: 'center'
-                                        }}
-                                    >
-                                        Submit
-                                    </Button>
-                                    <Button
-                                        variant="contained"
-                                        color="info"
-                                        style={{
-                                            margin: 10,
-                                            width: '50%'
-                                        }}
-                                        onClick={() => setOpenModal(!openModal)}
-                                    >
-                                        Cancel
-                                    </Button>
-                                </Box>
-                            </Form>
-                        )}
-                    </Formik>
+            <FullScreenDialog title="Add New Game Group" dialogStatus={openModal} setDialogStatus={setOpenModal} formik={formik}>
+                <Box style={{ padding: 30 }}>
+                    <TextField
+                        value={formik.values.name}
+                        type="text"
+                        name="name"
+                        label="Game Group Name"
+                        onChange={formik.handleChange}
+                        variant="outlined"
+                        fullWidth
+                        style={{ marginTop: 10, marginBottom: 10 }}
+                        error={formik.touched.name && Boolean(formik.errors.name)}
+                        helperText={formik.touched.name && formik.errors.name}
+                        required
+                    />
+                    <TextField
+                        value={formik.values.description}
+                        multiline
+                        rows={10}
+                        type="text"
+                        name="description"
+                        label="Description"
+                        onChange={formik.handleChange}
+                        variant="outlined"
+                        fullWidth
+                        style={{ marginTop: 10, marginBottom: 10 }}
+                        error={formik.touched.description && Boolean(formik.errors.description)}
+                        helperText={formik.touched.description && formik.errors.description}
+                    />
                 </Box>
-            </Modal>
+            </FullScreenDialog>
         </Box>
     );
 }

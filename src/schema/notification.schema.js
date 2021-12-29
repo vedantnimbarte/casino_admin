d77fb5197654f75420f610b689054295ace5e7e6
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-29 10:21:32 +0530

    Updated notification schema, now image and video url is required only when the type is image or video

--- src/schema/notification.schema.js
+++ src/schema/notification.schema.js
@@ -10,15 +10,21 @@ const notificationSchema = yup.object().shape({
         .required('Please select notification type'),
     title: yup.string().max(255, 'Notification title shoule be maximum 255 characters').required('Please enter the notification title'),
     message: yup.string().max(1024, 'Message should be maximum 1024 characters').required('Please enter the notification message'),
-    url: yup.string().url().max(255, 'Video URL should be maximum 255 characters'),
-    image: yup
-        .mixed()
-        .required('Please select image')
-        .test(
-            'filetype',
-            'Only .jpg, .jpeg, .png files are supported',
-            (value) => !value || (value && SUPPORTED_FILETYPES.includes(value.type))
-        )
+    url: yup.string().when('type', {
+        is: 'video',
+        then: yup.string().url().max(255, 'Video URL should be maximum 255 characters').required('Please enter video url')
+    }),
+    image: yup.mixed().when('type', {
+        is: 'image',
+        then: yup
+            .mixed()
+            .required('Please select image')
+            .test(
+                'filetype',
+                'Only .jpg, .jpeg, .png files are supported',
+                (value) => !value || (value && SUPPORTED_FILETYPES.includes(value.type))
+            )
+    })
 });
 
 export default notificationSchema;

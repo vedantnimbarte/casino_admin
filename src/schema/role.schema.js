fd444e862a3fff95a56c4d493d8d04441a532fb2
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-29 18:55:35 +0530

    Fixed role schema and added parenr role, and description schema

--- src/schema/role.schema.js
+++ src/schema/role.schema.js
@@ -1,7 +1,9 @@
 import * as yup from 'yup';
 
 const roleSchema = yup.object().shape({
-    name: yup.string().max(100, 'Role name should be maximum 100 characters').required('Please enter the role name')
+    name: yup.string().max(100, 'Role name should be maximum 100 characters').required('Please enter the role name'),
+    parentRole: yup.string().required('Please select parent role'),
+    description: yup.string().optional().max(50, 'Role description should be maximum 50 characters')
 });
 
 export default roleSchema;

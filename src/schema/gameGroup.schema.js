590005acb7cee6c75ced04c9c41e32aa0e74464a
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-30 15:39:09 +0530

    Updated game group description max value in game group schema

--- src/schema/gameGroup.schema.js
+++ src/schema/gameGroup.schema.js
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 
 const gameGroupSchema = yup.object().shape({
     name: yup.string().max(100, 'Name should be maximum 255 characters').required('Please enter the name of game group'),
-    description: yup.string().optional().max(255, 'Description should be maximum 255 characters')
+    description: yup.string().optional().max(100, 'Description should be maximum 100 characters')
 });
 
 export default gameGroupSchema;

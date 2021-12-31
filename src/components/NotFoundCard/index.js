35ce76403fe75afab28e1dce7e3b1e2fd6902a8e
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-31 10:57:08 +0530

    Added Icon to no found card component

--- src/components/NotFoundCard/index.js
+++ src/components/NotFoundCard/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Paper, Typography } from '@mui/material';
+import { IconFilesOff as NoDataIcon } from '@tabler/icons';
 
 function NotFoundCard({ msg }) {
     return (
         <Paper style={{ display: 'flex', height: 60, justifyContent: 'center', alignItems: 'center' }} fullWidth>
+            <NoDataIcon />
             <Typography style={{ fontWeight: 900, fontSize: 20 }}>{msg}</Typography>
         </Paper>
     );

0f14b9e86e588bb7c2599471d1dff4c1cefe4153
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-31 11:26:59 +0530

    Added address field and multiselect field for game type and permissions

--- src/views/pages/network/components/Forms/NewAgent.js
+++ src/views/pages/network/components/Forms/NewAgent.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import { Box, TextField, MenuItem, Grid, Button, useTheme } from '@mui/material';
+import { Box, TextField, MenuItem, Grid, Button, useTheme, Chip, Select, FormControl, InputLabel, OutlinedInput } from '@mui/material';
 import { IconDeviceFloppy as SaveIcon, IconRefresh as ResetIcon } from '@tabler/icons';
 
 function NewPlayerForm({ formik, onClose, openModal }) {
     const theme = useTheme();
+    const [gameType, setGameType] = React.useState([]);
+    const [permissions, setPermissions] = React.useState([]);
+
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250
+            }
+        }
+    };
+
+    const gameTypesList = ['All', 'Fish', 'Table', 'Slot'];
+    const permissionsList = ['All'];
+
+    function getStyles(name, personName, theme) {
+        return {
+            fontWeight: personName.indexOf(name) === -1 ? theme.typography.fontWeightRegular : theme.typography.fontWeightMedium
+        };
+    }
+
+    const handleGameTypeChange = (event) => {
+        const {
+            target: { value }
+        } = event;
+        setGameType(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value
+        );
+    };
+    const handlePermissionsChange = (event) => {
+        const {
+            target: { value }
+        } = event;
+        setPermissions(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value
+        );
+    };
 
     return (
         <Box style={{ display: 'flex', flexDirection: 'column' }}>
@@ -92,10 +133,25 @@ function NewPlayerForm({ formik, onClose, openModal }) {
                     required
                 />
                 <TextField
+                    value={formik.values.address}
+                    type="text"
+                    name="address"
+                    label="Address"
+                    onChange={formik.handleChange}
+                    variant="outlined"
+                    multiline
+                    rows={5}
+                    fullWidth
+                    style={{ marginTop: 10, marginBottom: 10 }}
+                    error={formik.touched.address && Boolean(formik.errors.address)}
+                    helperText={formik.touched.address && formik.errors.address}
+                    required
+                />
+                <TextField
                     value={formik.values.agent}
                     select
                     name="agent"
-                    label="Select Agent"
+                    label="Select Agent Type"
                     onChange={formik.handleChange}
                     variant="outlined"
                     fullWidth
@@ -108,7 +164,68 @@ function NewPlayerForm({ formik, onClose, openModal }) {
                     <MenuItem value="distributor">Distributor</MenuItem>
                     <MenuItem value="sub_distributor">Sub Distributor</MenuItem>
                     <MenuItem value="store">Store</MenuItem>
+                    <MenuItem value="cashior">Cashior</MenuItem>
                 </TextField>
+                <Grid container>
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                        <FormControl fullWidth style={{ marginTop: 10, marginBottom: 10 }}>
+                            <InputLabel id="demo-multiple-chip-label">Select Game Type Permissions</InputLabel>
+                            <Select
+                                labelId="demo-multiple-chip-label"
+                                id="demo-multiple-chip"
+                                multiple
+                                fullWidth
+                                value={gameType}
+                                style={{ width: '99%' }}
+                                onChange={handleGameTypeChange}
+                                input={<OutlinedInput id="select-multiple-chip" label="Select Game Type Permissions" />}
+                                renderValue={(selected) => (
+                                    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                                        {selected.map((value) => (
+                                            <Chip key={value} label={value} />
+                                        ))}
+                                    </Box>
+                                )}
+                                MenuProps={MenuProps}
+                            >
+                                {gameTypesList.map((gameType) => (
+                                    <MenuItem key={gameType} value={gameType} style={getStyles(gameType, gameType, theme)}>
+                                        {gameType}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                        <FormControl fullWidth style={{ marginTop: 10, marginBottom: 10 }}>
+                            <InputLabel id="demo-multiple-chip-label">Select Permissions</InputLabel>
+                            <Select
+                                labelId="demo-multiple-chip-label"
+                                id="demo-multiple-chip"
+                                multiple
+                                fullWidth
+                                style={{ width: '99%' }}
+                                value={permissions}
+                                onChange={handlePermissionsChange}
+                                input={<OutlinedInput id="select-multiple-chip" label="Select Permissions" />}
+                                renderValue={(selected) => (
+                                    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                                        {selected.map((value) => (
+                                            <Chip key={value} label={value} />
+                                        ))}
+                                    </Box>
+                                )}
+                                MenuProps={MenuProps}
+                            >
+                                {permissionsList.map((permission) => (
+                                    <MenuItem key={permission} value={permission} style={getStyles(permission, permissions, theme)}>
+                                        {permission}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                </Grid>
                 <Box style={{ display: 'flex', justifyContent: 'right' }}>
                     <Button
                         type="reset"

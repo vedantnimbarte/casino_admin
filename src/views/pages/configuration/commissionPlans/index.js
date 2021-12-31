cfa1d90158a096d1aa565ed126eb07e2878d0e57
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-31 10:10:14 +0530

    Added buttons for save and discard changes and passing title to commission card

--- src/views/pages/configuration/commissionPlans/index.js
+++ src/views/pages/configuration/commissionPlans/index.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { IconButton, Tooltip, Box, Button, Grid, InputLabel, TextField, FormControl, Select, MenuItem, Typography } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 // import DataTable from 'mui-datatables';
-import { IconEditCircle as EditIcon, IconHistory as HistoryIcon } from '@tabler/icons';
+import { IconEditCircle as EditIcon, IconHistory as HistoryIcon, IconEdit as SaveIcon, IconRefresh as ResetIcon } from '@tabler/icons';
 
 // Components
 import MainCard from '../../../../ui-component/cards/MainCard';
@@ -20,32 +20,32 @@ function CommissionPlans() {
             <MainCard
                 title="Comission Plans"
                 secondary={
-                    <Tooltip title="Edit Commission Plan">
-                        <IconButton onClick={() => setOpenModal(!openModal)}>
-                            <EditIcon />
-                        </IconButton>
-                    </Tooltip>
+                    <Box>
+                        <Button startIcon={<HistoryIcon />} onClick={() => setCommissionHistory(!commissionHistory)} color="secondary">
+                            Commission History
+                        </Button>
+                        <Button startIcon={<SaveIcon />} variant="contained" color="primary" sx={{ mx: 3 }}>
+                            Save Changes
+                        </Button>
+                        <Button startIcon={<ResetIcon />} variant="contained" color="error">
+                            Discard Changes
+                        </Button>
+                    </Box>
                 }
             >
                 <Box>
-                    <Box style={{ display: 'flex', justifyContent: 'right', marginBottom: 10 }} fullWidth>
-                        <Button
-                            startIcon={<HistoryIcon />}
-                            style={{ color: 'gray' }}
-                            onClick={() => setCommissionHistory(!commissionHistory)}
-                        >
-                            Commission History
-                        </Button>
-                    </Box>
                     <Grid container spacing={4}>
                         <Grid item lg={4} md={6} sm={6} xs={12}>
-                            <CommissionCard />
+                            <CommissionCard title="Master Distributor" />
                         </Grid>
                         <Grid item lg={4} md={6} sm={6} xs={12}>
-                            <CommissionCard />
+                            <CommissionCard title="Distributor" />
                         </Grid>
                         <Grid item lg={4} md={6} sm={6} xs={12}>
-                            <CommissionCard />
+                            <CommissionCard title="Sub Distributor" />
+                        </Grid>
+                        <Grid item lg={4} md={6} sm={6} xs={12}>
+                            <CommissionCard title="Stores" />
                         </Grid>
                     </Grid>
                     {/* <DataTable title="Commission Plans" data={data} columns={columns} options={options} /> */}

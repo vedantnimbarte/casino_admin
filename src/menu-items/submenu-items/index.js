8cd73150058749cac16d9a04b8d4db24351d72db
vedant-nimbarte <vedant.mountgames@gmail.com>
2021-12-31 18:04:40 +0530

    Added network menu items to submenu items index

--- src/menu-items/submenu-items/index.js
+++ src/menu-items/submenu-items/index.js
@@ -1,4 +1,5 @@
 import configurationMenuItems from './configuration';
 import cmsMenuItems from './cms';
+import networkMenuItems from './network';
 
-export { configurationMenuItems, cmsMenuItems };
+export { configurationMenuItems, cmsMenuItems, networkMenuItems };

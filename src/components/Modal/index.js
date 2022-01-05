0d0200df58132c1e163bc6b7b1e9de406a9d78a4
vedant-nimbarte <vedant.mountgames@gmail.com>
2022-01-05 20:31:15 +0530

    SHowing modal based on device type'

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,67 +1,59 @@
 import propTypes from 'prop-types';
 import { useEffect, useState } from 'react';
-import { Modal, IconButton, useMediaQuery, useTheme, Grid } from '@mui/material';
+import { Modal, IconButton, useMediaQuery, useTheme, Grid, Typography } from '@mui/material';
 import MainCard from '../../ui-component/cards/MainCard';
 import { IconX as CloseIcon } from '@tabler/icons';
-import { isMobile } from 'react-device-detect';
+import FullScreenDialog from 'components/FullScreenDialog';
+import { isMobile, isTablet, isMobileOnly } from 'react-device-detect';
 
 function ModalComponent({ title, children, open, onClose }) {
     const theme = useTheme();
-    const isXSMobileDevice = useMediaQuery(theme.breakpoints.down('xs'));
     const isMobileDevice = useMediaQuery(theme.breakpoints.down('sm'));
-    const isTabletDevice = useMediaQuery(theme.breakpoints.down('md'));
-    const isDesktoptDevice = useMediaQuery(theme.breakpoints.up('lg'));
-    const isXDesktoptDevice = useMediaQuery(theme.breakpoints.down('xl'));
-    // const isNotebookDevice = useMediaQuery('(width:1024px)');
-    const [deviceStyleProperties, setDeviceStyleProperties] = useState({ maxWidth: '50%' });
-
-    useEffect(() => {
-        if (isXSMobileDevice) {
-            setDeviceStyleProperties({ width: '90%', transform: 'translate(-50%, -35%)' });
-        } else if (isMobileDevice) {
-            setDeviceStyleProperties({ width: '98%', transform: 'translate(-50%, -35%)' });
-        } else if (isTabletDevice) {
-            setDeviceStyleProperties({ transform: 'translate(-50%, -50%)' });
-        } else if (isDesktoptDevice) {
-            setDeviceStyleProperties({ transform: 'translate(-50%, -50%)' });
-        } else if (isXDesktoptDevice) {
-            setDeviceStyleProperties({ transform: 'translate(-50%, -50%)' });
-        }
-    }, [isMobileDevice, isTabletDevice, isDesktoptDevice, isXDesktoptDevice, isXSMobileDevice]);
 
     return (
-        <Modal
-            open={open}
-            onClose={onClose}
-            sx={{
-                overflowY: 'auto',
-                display: 'block',
-                overflow: 'scroll'
-            }}
-        >
-            <Grid xs={6} sm={6} md={6} lg={6}>
-                <MainCard
-                    title={title}
-                    style={{
-                        margin: 0,
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        ...deviceStyleProperties
-                    }}
-                    secondary={
-                        <IconButton onClick={onClose}>
-                            <CloseIcon />
-                        </IconButton>
-                    }
-                >
+        <>
+            {(isMobileOnly || isTablet) && (
+                <FullScreenDialog title={title} dialogStatus={open} setDialogStatus={onClose}>
                     {children}
-                </MainCard>
-            </Grid>
-        </Modal>
+                    <Typography>{isMobileDevice}</Typography>
+                </FullScreenDialog>
+            )}
+            {!isTablet && !isMobileOnly && (
+                <Modal
+                    open={open}
+                    onClose={onClose}
+                    sx={{
+                        overflowY: 'auto',
+                        display: 'block',
+                        overflow: 'scroll'
+                    }}
+                >
+                    <Grid xs={6} sm={6} md={6} lg={6}>
+                        <MainCard
+                            title={title}
+                            style={{
+                                margin: 0,
+                                position: 'absolute',
+                                top: '50%',
+                                left: '50%',
+                                transform: 'translate(-50%, -50%)'
+                            }}
+                            secondary={
+                                <IconButton onClick={onClose}>
+                                    <CloseIcon />
+                                </IconButton>
+                            }
+                        >
+                            {children}
+                            <Typography>{isMobileDevice}</Typography>
+                        </MainCard>
+                    </Grid>
+                </Modal>
+            )}
+            );
+        </>
     );
 }
-
 ModalComponent.propTypes = {
     title: propTypes.string.isRequired,
     open: propTypes.string.isRequired,

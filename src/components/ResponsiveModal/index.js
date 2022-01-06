d75bb9d45101357a8a6af29431ec287fb1077471
vedant-nimbarte <vedant.mountgames@gmail.com>
2022-01-06 10:45:31 +0530

    Created new responsive modal

---
+++ src/components/ResponsiveModal/index.js
@@ -0,0 +1,75 @@
+import propTypes from 'prop-types';
+import { Modal, IconButton, useMediaQuery, useTheme, Grid, Typography } from '@mui/material';
+import MainCard from '../../ui-component/cards/MainCard';
+import { IconX as CloseIcon } from '@tabler/icons';
+import FullScreenDialog from 'components/FullScreenDialog';
+import { BrowserView, MobileView, TabletView, isIOS, isTablet } from 'react-device-detect';
+
+function ModalComponent({ title, children, open, onClose }) {
+    const theme = useTheme();
+    const isMobileDevice = useMediaQuery(theme.breakpoints.down('sm'));
+    // console.log(window.screen.orientation.angle);
+    return (
+        <>
+            {/* TABLET VIEW */}
+            <TabletView>
+                <FullScreenDialog title={title} dialogStatus={open} setDialogStatus={onClose}>
+                    {children}
+                    <Typography>{isMobileDevice}</Typography>
+                </FullScreenDialog>
+            </TabletView>
+
+            {/* MOBILE VIEW */}
+            <MobileView>
+                <FullScreenDialog title={title} dialogStatus={open} setDialogStatus={onClose}>
+                    {children}
+                    <Typography>{isMobileDevice}</Typography>
+                </FullScreenDialog>
+            </MobileView>
+
+            {/* BROWSER VIEW */}
+            <BrowserView>
+                {(!isIOS || !isTablet) && (
+                    <Modal
+                        open={open}
+                        onClose={onClose}
+                        sx={{
+                            overflowY: 'auto',
+                            display: 'block',
+                            overflow: 'scroll'
+                        }}
+                    >
+                        <Grid xs={6} sm={6} md={6} lg={6}>
+                            <MainCard
+                                title={title}
+                                style={{
+                                    margin: 0,
+                                    position: 'absolute',
+                                    top: '50%',
+                                    left: '50%',
+                                    transform: 'translate(-50%, -50%)'
+                                }}
+                                secondary={
+                                    <IconButton onClick={onClose}>
+                                        <CloseIcon />
+                                    </IconButton>
+                                }
+                            >
+                                {children}
+                                <Typography>{isMobileDevice}</Typography>
+                            </MainCard>
+                        </Grid>
+                    </Modal>
+                )}
+            </BrowserView>
+        </>
+    );
+}
+ModalComponent.propTypes = {
+    title: propTypes.string.isRequired,
+    open: propTypes.string.isRequired,
+    onClose: propTypes.func.isRequired,
+    children: propTypes.any.isRequired
+};
+
+export default ModalComponent;

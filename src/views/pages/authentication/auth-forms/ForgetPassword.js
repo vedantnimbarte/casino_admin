e1519f4ffd9923930cc65af830eefea073293413
vedant-nimbarte <vedant.mountgames@gmail.com>
2022-01-06 14:50:46 +0530

    Created forget password screen

---
+++ src/views/pages/authentication/auth-forms/ForgetPassword.js
@@ -0,0 +1,131 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import {
+    Box,
+    Button,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormHelperText,
+    Grid,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
+    Stack,
+    Typography
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+// third party
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+
+// project imports
+import useScriptRef from 'hooks/useScriptRef';
+import AnimateButton from 'ui-component/extended/AnimateButton';
+
+// assets
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+// ============================|| FIREBASE - LOGIN ||============================ //
+
+const FirebaseLogin = ({ ...others }) => {
+    const theme = useTheme();
+    const user = useSelector((state) => state.user);
+    const navigate = useNavigate();
+    const scriptedRef = useScriptRef();
+    const [checked, setChecked] = useState(true);
+
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <>
+            <Formik
+                initialValues={{
+                    email: user.email,
+                    password: '123456',
+                    submit: null
+                }}
+                validationSchema={Yup.object().shape({
+                    email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+                    password: Yup.string().max(255).required('Password is required')
+                })}
+                onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+                    try {
+                        if (scriptedRef.current) {
+                            setStatus({ success: true });
+                            setSubmitting(false);
+                            navigate('/dashboard');
+                        }
+                    } catch (err) {
+                        console.error(err);
+                        if (scriptedRef.current) {
+                            setStatus({ success: false });
+                            setErrors({ submit: err.message });
+                            setSubmitting(false);
+                        }
+                    }
+                }}
+            >
+                {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+                    <form noValidate onSubmit={handleSubmit} {...others}>
+                        <FormControl fullWidth error={Boolean(touched.email && errors.email)} sx={{ ...theme.typography.customInput }}>
+                            <InputLabel htmlFor="outlined-adornment-email-login">Email Address</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-email-login"
+                                type="email"
+                                value={values.email}
+                                name="email"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                label="Email Address / Username"
+                                inputProps={{}}
+                            />
+                            {touched.email && errors.email && (
+                                <FormHelperText error id="standard-weight-helper-text-email-login">
+                                    {errors.email}
+                                </FormHelperText>
+                            )}
+                        </FormControl>
+
+                        {errors.submit && (
+                            <Box sx={{ mt: 3 }}>
+                                <FormHelperText error>{errors.submit}</FormHelperText>
+                            </Box>
+                        )}
+
+                        <Box sx={{ mt: 2 }}>
+                            <AnimateButton>
+                                <Button
+                                    disableElevation
+                                    disabled={isSubmitting}
+                                    fullWidth
+                                    size="large"
+                                    type="submit"
+                                    variant="contained"
+                                    color="secondary"
+                                >
+                                    Submit
+                                </Button>
+                            </AnimateButton>
+                        </Box>
+                    </form>
+                )}
+            </Formik>
+        </>
+    );
+};
+
+export default FirebaseLogin;
---
+++ src/views/pages/authentication/authentication3/ForgetPassword.js
@@ -0,0 +1,86 @@
+import { Link } from 'react-router-dom';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Divider, Grid, Stack, Typography, useMediaQuery } from '@mui/material';
+
+// project imports
+import AuthWrapper1 from '../AuthWrapper1';
+import AuthCardWrapper from '../AuthCardWrapper';
+import ForgetPasswordForm from '../auth-forms/ForgetPassword';
+import Logo from 'ui-component/Logo';
+
+// assets
+
+// ================================|| AUTH3 - LOGIN ||================================ //
+
+const ForgetPassword = () => {
+    const theme = useTheme();
+    const matchDownSM = useMediaQuery(theme.breakpoints.down('md'));
+
+    return (
+        <AuthWrapper1>
+            <Grid container direction="column" justifyContent="flex-end" sx={{ minHeight: '100vh' }}>
+                <Grid item xs={12}>
+                    <Grid container justifyContent="center" alignItems="center" sx={{ minHeight: 'calc(100vh - 68px)' }}>
+                        <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
+                            <AuthCardWrapper>
+                                <Grid container spacing={2} alignItems="center" justifyContent="center">
+                                    <Grid item sx={{ mb: 3 }}>
+                                        <Link to="#">
+                                            <Logo />
+                                        </Link>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Grid
+                                            container
+                                            direction={matchDownSM ? 'column-reverse' : 'row'}
+                                            alignItems="center"
+                                            justifyContent="center"
+                                        >
+                                            <Grid item>
+                                                <Stack alignItems="center" justifyContent="center" spacing={1}>
+                                                    <Typography
+                                                        color={theme.palette.secondary.main}
+                                                        gutterBottom
+                                                        variant={matchDownSM ? 'h3' : 'h2'}
+                                                    >
+                                                        Hi, Forget password?
+                                                    </Typography>
+                                                </Stack>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <ForgetPasswordForm />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Divider />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Grid item container direction="column" alignItems="center" xs={12}>
+                                            <Typography
+                                                component={Link}
+                                                // to="/pages/register/register3"
+                                                to="/"
+                                                variant="subtitle1"
+                                                sx={{ textDecoration: 'none' }}
+                                            >
+                                                Go to login
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </AuthCardWrapper>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                {/* <Grid item xs={12} sx={{ m: 3, mt: 1 }}>
+                    <AuthFooter />
+                </Grid> */}
+            </Grid>
+        </AuthWrapper1>
+    );
+};
+
+export default ForgetPassword;

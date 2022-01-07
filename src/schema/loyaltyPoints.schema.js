af7f40a9a363f0827eef20b2324b5ae8bfcf5356
vedant-nimbarte <vedant.mountgames@gmail.com>
2022-01-07 18:43:08 +0530

    Removed wagering value check from loyalty points schema

--- src/schema/loyaltyPoints.schema.js
+++ src/schema/loyaltyPoints.schema.js
@@ -3,8 +3,7 @@ import * as yup from 'yup';
 const loyaltyPointsSchema = yup.object().shape({
     level: yup.string().required('Please enter loyalty level'),
     pointsNeeded: yup.string().required('Please enter points needed for loyalty level'),
-    multiplier: yup.number('Multiplier should be a number').required('Please enter multiplier for loyalty level'),
-    wageringValue: yup.string().required('Please enter wagering value')
+    multiplier: yup.number('Multiplier should be a number').required('Please enter multiplier for loyalty level')
 });
 
 export default loyaltyPointsSchema;

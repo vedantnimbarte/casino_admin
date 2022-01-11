322fd53a84126facbcbd0b9970cad9d90e87ddc3
vedant-nimbarte <vedant.mountgames@gmail.com>
2022-01-11 17:56:39 +0530

    Added regular expression to agent type name validation

--- src/schema/role.schema.js
+++ src/schema/role.schema.js
@@ -1,7 +1,13 @@
 import * as yup from 'yup';
 
+const ROLENAME_REGEX = /^[A-Za-z][A-Za-z-_]+$/;
+
 const roleSchema = yup.object().shape({
-    name: yup.string().max(100, 'Role name should be maximum 100 characters').required('Please enter the role name'),
+    name: yup
+        .string()
+        .matches(ROLENAME_REGEX, 'Agent type should start only with letters and contain only letters, dashes and underscores')
+        .max(100, 'Role name should be maximum 100 characters')
+        .required('Please enter the role name'),
     parentRole: yup.string().required('Please select parent role'),
     description: yup.string().optional().max(50, 'Role description should be maximum 50 characters')
 });

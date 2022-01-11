7e03c66e06f8501f1467dd0a954e40d09f676d4f
vedant-nimbarte <vedant.mountgames@gmail.com>
2022-01-11 17:55:52 +0530

    Added subroutes class to constants

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,7 +1,13 @@
+/* eslint:disable:max-classes-per-file */
+
 const API_URL = 'http://192.168.29.78:3002/admin';
 
-export class InternalAPI {
+class InternalAPI {
     static AGENT = '/role';
 }
 
-export { API_URL };
+class SubRoutes {
+    static LIST = '/list';
+}
+
+export { API_URL, InternalAPI, SubRoutes };

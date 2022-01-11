96c66fd6f8193e06422f77e298b347b73bf2d69a
vedant-nimbarte <vedant.mountgames@gmail.com>
2022-01-11 17:57:48 +0530

    Added create read update delete thunk

--- src/store/reducers/configuration/agentType.reducer.js
+++ src/store/reducers/configuration/agentType.reducer.js
@@ -1,31 +1,91 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { createAgentType } from 'store/thunk/configuration/agentType.thunk';
+import { createAgentType, deleteAgentType, getAgentType, updateAgentType } from 'store/thunk/configuration/agentType.thunk';
 
 const AgentTypeSlice = createSlice({
     name: 'agent_type',
     initialState: {
         status: null,
-        errorMsg: '',
+        msg: '',
+        totalRecords: 0,
         data: []
     },
     extraReducers: {
+        // Get Agent Type Reducers
+        [getAgentType.pending]: (state) => {
+            state.status = 'loading';
+        },
+        [getAgentType.fulfilled]: (state, { payload }) => {
+            if (payload.status === true) {
+                state.msg = payload.msg;
+                state.data = payload.data;
+                state.totalRecords = payload.total;
+                state.status = 'success';
+            }
+            if (payload.status === false) {
+                state.msg = payload.msg || 'Network Error';
+                state.status = 'failed';
+            }
+        },
+        [getAgentType.rejected]: (state) => {
+            state.status = 'failed';
+            state.msg = 'Something went wrong. Please try again.';
+        },
+
         // Create Agent Type Reducers
         [createAgentType.pending]: (state) => {
             state.status = 'loading';
         },
         [createAgentType.fulfilled]: (state, { payload }) => {
             if (payload.status === true) {
-                state.data = payload;
+                state.msg = payload.msg;
                 state.status = 'success';
-            } else {
-                state.data = payload.message;
+            }
+            if (payload.status === false) {
+                state.msg = payload.msg || 'Network Error';
                 state.status = 'failed';
             }
         },
-        [createAgentType.rejected]: (state, { payload }) => {
-            state.data = payload;
+        [createAgentType.rejected]: (state) => {
             state.status = 'failed';
-            state.errorMsg = 'Something went wrong. Please try again.';
+            state.msg = 'Something went wrong. Please try again.';
+        },
+
+        // Update Agent Type Reducers
+        [updateAgentType.pending]: (state) => {
+            state.status = 'loading';
+        },
+        [updateAgentType.fulfilled]: (state, { payload }) => {
+            if (payload.status === true) {
+                state.msg = payload.msg || 'Network Error';
+                state.status = 'success';
+            }
+            if (payload.status === false) {
+                state.msg = payload.msg || 'Network not available';
+                state.status = 'failed';
+            }
+        },
+        [updateAgentType.rejected]: (state) => {
+            state.status = 'failed';
+            state.msg = 'Something went wrong. Please try again.';
+        },
+
+        // Delete Agent Type Reducers
+        [deleteAgentType.pending]: (state) => {
+            state.status = 'loading';
+        },
+        [deleteAgentType.fulfilled]: (state, { payload }) => {
+            if (payload.status === true) {
+                state.msg = payload.msg;
+                state.status = 'success';
+            }
+            if (payload.status === false) {
+                state.msg = payload.msg || 'Network not available';
+                state.status = 'failed';
+            }
+        },
+        [deleteAgentType.rejected]: (state) => {
+            state.status = 'failed';
+            state.msg = 'Something went wrong. Please try again.';
         }
     }
 });
--- src/store/thunk/configuration/agentType.thunk.js
+++ src/store/thunk/configuration/agentType.thunk.js
@@ -1,25 +1,49 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { API_URL, InternalAPI } from 'common/constants';
-import { AgentType } from 'common/serializeData';
+import { API_URL, InternalAPI, SubRoutes } from 'common/constants';
 
-export const createAgentType = createAsyncThunk('agent_type/createAgentType', async (data) => {
+// CREATE AGENT TYPE THUNK
+export const createAgentType = createAsyncThunk('agent_type/createAgentType', async ({ name, parentRole, description }) => {
     const requestOptions = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(AgentType(data))
+        body: JSON.stringify({ ROLE_NAME: name, ROLE_PARENT_ID: parentRole })
     };
     return fetch(`${API_URL}${InternalAPI.AGENT}`, requestOptions).then((res) => res.json());
 });
 
-export const dcreateAgentType = createAsyncThunk('agent_type/createAgentType', async (data) => {
+// GET AGENT TYPE LIST THUNK
+export const getAgentType = createAsyncThunk('agent_type/getAgentType', async ({ pageno, limit }) => {
     const requestOptions = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(AgentType(data))
+        body: JSON.stringify({ pageNumber: pageno - 1, pageLimit: limit })
     };
-    return fetch(`${API_URL}${InternalAPI.AGENT}`, requestOptions).then((res) => res.json());
+    return fetch(`${API_URL}${InternalAPI.AGENT}${SubRoutes.LIST}`, requestOptions).then((res) => res.json());
+});
+
+// UPDATE AGENT TYPE THUNK
+export const updateAgentType = createAsyncThunk('agent_type/updateAgentType', async ({ name, parentRole, description, id }) => {
+    const requestOptions = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ ROLE_NAME: name, ROLE_PARENT_ID: parentRole, DESCRIPTION: description.length > 0 && description })
+    };
+    return fetch(`${API_URL}${InternalAPI.AGENT}/${id}`, requestOptions).then((res) => res.json());
+});
+
+// DELETE AGENT TYPE THUNK
+export const deleteAgentType = createAsyncThunk('agent_type/deleteAgentType', async ({ id }) => {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    return fetch(`${API_URL}${InternalAPI.AGENT}/${id}`, requestOptions).then((res) => res.json());
 });

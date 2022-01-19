d7b398044e31f996f8a4012666262c94abccc26c
vedant-nimbarte <vedant.mountgames@gmail.com>
2022-01-19 13:10:31 +0530

    SHowing agent types in dropdown list and getting list from redux store on api call

--- src/views/pages/configuration/menuPermission/index.js
+++ src/views/pages/configuration/menuPermission/index.js
@@ -1,11 +1,16 @@
-import { Box } from '@mui/material';
+import { Box, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
 
 // Components
 import MainCard from '../../../../ui-component/cards/MainCard';
 import DataTable from 'components/DataTable';
 import NotFoundCard from 'components/NotFoundCard';
+import { useEffect } from 'react';
+import { getAgentTypesList } from 'store/thunk/configuration/agentType.thunk';
 
 function MenuPermissions() {
+    const agentType = useSelector((state) => state.agentType);
+    const dispatch = useDispatch();
     const columns = ['ID', 'Permission', 'Allocated', 'Action'];
 
     const data = [];
@@ -20,9 +25,27 @@ function MenuPermissions() {
         rowsPerPageOptions: [10, 20]
     };
 
+    useEffect(() => {
+        dispatch(getAgentTypesList());
+    }, []);
+
     return (
         <Box>
-            <MainCard title="Menu Permissions">
+            <MainCard
+                title="Menu Permissions"
+                secondary={
+                    <FormControl fullWidth>
+                        <InputLabel htmlFor="agent">Select Agent Type</InputLabel>
+                        <Select value="agent1" label="Select Agent Type" name="agent" id="agent">
+                            {agentType.agentTypesList?.map((parentAgentType, index) => (
+                                <MenuItem value={parentAgentType.ROLE_ID} key={index}>
+                                    {parentAgentType.ROLE_NAME}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                }
+            >
                 <Box>
                     {data.length > 0 ? (
                         <DataTable title="Menu Permissions List" data={data} columns={columns} options={options} />

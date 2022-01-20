41d68df9cb79f09b2b4b994e520e74beadd13f96
vedant-nimbarte <vedant.mountgames@gmail.com>
2022-01-20 14:47:46 +0530

    Added coin pack static url

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,13 +1,13 @@
 /* eslint:disable:max-classes-per-file */
 
-// const API_URL = 'http://192.168.29.78:3002/admin';
-const API_URL = 'http://143.110.183.1:3002/admin';
+const API_URL = 'http://192.168.29.78:3002/admin';
+// const API_URL = 'http://143.110.183.1:3002/admin';
 
 class InternalAPI {
     static AGENT = '/agenttype';
     static GAMEGROUP = '/gametype';
     static FAQ = '/faq';
-    static GAMEPACK = '/gamepack';
+    static COINPACK = '/coinpack';
     static SETTINGS = '/setting';
 }
 
